4b43fa6ace25ae18273d72c2ebd45c2bf0984887
王某人 <48933649+WangKeDS@users.noreply.github.com>
2019-05-07 16:16:30 +0800

    上传图片

---
+++ erweima.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<form action="PD.php" method="post" enctype="multipart/form-data">
+<label for="file">上传文件:</label>
+<input type="file" name="file" id="file" /> 
+<input type="submit" name="submit" value="上传" />
+</form>
+</body>
+</html>
+
